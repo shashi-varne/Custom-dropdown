45f4da572ffa6cd5932226d751cc97d8bf7e3c7f
shashi-varne <shashi.varne04@gmail.com>
2022-02-07 21:26:10 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+import Select from "./select.js";
+
+const selectElements = document.querySelectorAll("[data-custom]");
+selectElements.forEach((selectElement) => {
+  new Select(selectElement);
+});
